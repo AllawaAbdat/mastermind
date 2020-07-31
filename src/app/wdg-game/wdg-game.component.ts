45f56c8e012c5efcb1259bb28b65fd4515ed8704
AllawaAbdat <allawa.abdat@laposte.net>
2020-07-31 08:03:38 +0100

    ...

--- src/app/wdg-game/wdg-game.component.ts
+++ src/app/wdg-game/wdg-game.component.ts
@@ -15,6 +15,8 @@ export class WdgGameComponent implements OnInit, AfterViewChecked, AfterViewInit
   // Variables
   // Tab Colors used to compare with color of user
   colors = ['red', 'yellow', 'blue', 'orange', 'green', 'black'];
+  tempArray = [];
+  hintArray = [];
 
   // tab where we will stock all the 4 color selected by the use
   selectedColors = [];
@@ -81,21 +83,22 @@ export class WdgGameComponent implements OnInit, AfterViewChecked, AfterViewInit
     this.selectedColors.push(color);
 
     // if the user has selected 4 colors, then
-    if (this.selectedColors.length == 4) {
+    if (this.selectedColors.length === 4) {
 
         // temp array logic used for the history
-        const tempArray = [];
+        this.tempArray = [];
+        this.tempArray.length = 0;
 
         this.selectedColors.forEach((item) => {
-          tempArray.push(item);
+          this.tempArray.push(item);
         });
-        this.selectedColorsToBindOnHints[this.guesses] = tempArray;
+        this.selectedColorsToBindOnHints[this.guesses] = this.tempArray;
 
         // we set the counters to 0 before calling calculateHints
         this.hintsHalf = 0;
         this.hintsFull = 0;
         // hintArray tab will contain the hints for the user
-        const hintArray = this.calculateHints(this.selectedColors);
+        this.hintArray = this.calculateHints(this.selectedColors);
 
         // counter guesses, already explained at the top of this file
         this.guesses++;
@@ -105,17 +108,17 @@ export class WdgGameComponent implements OnInit, AfterViewChecked, AfterViewInit
         if (this.guesses > 9) {
             alert('Perdu ! C\'est dommage !');
             this.selectedColors.length = 0;
-            hintArray.length = 0;
+            this.hintArray.length = 0;
             this.selectedColorsToBindOnHints.length = 0;
             this.hintsHistory.length = 0;
             this.guesses = 0;
         }
 
         // if all the hints into hintArray are equals to 'full' (which means all the colors are good and well placed) then alert victory
-        if (hintArray.every(hints => hints === 'full') && (hintArray.length === this.final.length)) {
+        if (this.hintArray.every(hints => hints === 'full') && (this.hintArray.length === this.final.length)) {
             alert('Gagné ! Bravo');
             this.selectedColors.length = 0;
-            hintArray.length = 0;
+            this.hintArray.length = 0;
             this.selectedColorsToBindOnHints.length = 0;
             this.hintsHistory.length = 0;
             this.guesses = 0;
